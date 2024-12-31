a492851eec576056642a974b055c154ab8aeabad
khup practice <khup.practice@gmail.com>
2024-12-31 16:12:36 +0530

    step : 6 ( Use this commad )

--- EmployeeMangmentService/EmployeeDbContext.cs
+++ EmployeeMangmentService/EmployeeDbContext.cs
@@ -11,5 +11,9 @@ namespace EmployeeMangmentService
         }
         public DbSet <Department> Departments { get; set; } //Departments will name of table
         public DbSet<Employee> Employees { get; set; } //Employees will name of table
+
+        // After this run the following command.
+        // 1. add-migration AddTable_TableName
+        // 2. update-database
     }
 }
