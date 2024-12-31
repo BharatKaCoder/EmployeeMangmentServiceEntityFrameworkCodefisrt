e7bfed256823c0fc6a54566eb23cd427038656fe
khup practice <khup.practice@gmail.com>
2024-12-31 15:26:49 +0530

    Step: 2

--- EmployeeMangmentService/EmployeeDbContext.cs
+++ EmployeeMangmentService/EmployeeDbContext.cs
@@ -1,6 +1,10 @@
-﻿namespace EmployeeMangmentService
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeMangmentService
 {
-    public class EmployeeDbContext
+    public class EmployeeDbContext : DbContext
     {
+        public DbSet <Department> Departments { get; set; } //Departments will name of table
+        public DbSet<Employee> Employees { get; set; } //Employees will name of table
     }
 }
